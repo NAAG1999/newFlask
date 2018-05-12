7ccdce63a4e8ecad4f8ffd903309799e9dd01d7a
Nilesh <agnilesh8199@gmail.com>
2018-05-13 00:28:59 +0530

    lesson-3

--- lesson2.py
+++ lesson2.py
@@ -1,27 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 #Routing/mapping : whenever user goes to home page, return's output is shown
 @app.route('/')
 def index():
-    return "This is the homepage"
+    return "Method used : %s" %request.method
 
-@app.route('/profile')
-def profile():
-    return "<h2>Hey There, Welcome</h2>"
+@app.route('/bacon',methods=['GET', 'POST'])
+def bacon():
+    return "Method used : %s" %request.method
 
-@app.route('/profile/ABC')
-def any():
-    return "<h2>Hey There, Welcome Again</h2>"
+#to use POST method
 
-@app.route('/post/<username>')
-def post(username):
-    return "<h2>Hey There %s</h2>" % username
 
-@app.route('/post/<int:post_id>')
-def post_1(post_id):
-    return "<h2>Hey There id no %s</h2>" % post_id
 
 if __name__ == "__main__":
     app.run(debug=True) #kickoff webserver : starts the app
